56a7e9872b46f414b23c920d598c36f658f37180
Muhammad Awais <162321104+mhdawaisrajput@users.noreply.github.com>
2024-11-07 13:08:39 +0500

    responsive footer added

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,15 +5,18 @@ export default function Footer() {
   return (
     // Footer Section
     <footer className="text[#03045E] bg-[#F7F197] py-6 mt-36 font-semibold max-w-screen-2xl mx-auto">
-      <div className="mx-32 flex justify-between items-center">
-        <div className="text-sm">
+      <div className="mx-4 md:mx-32 flex flex-col md:flex-row justify-center md:justify-between items-center">
+        <div className="text-sm text-center md:text-left">
           &copy; {new Date().getFullYear()} Muhammad Awais. All rights reserved.
         </div>
-        <div className="flex space-x-6">
+        <div className="hidden md:block space-x-6">
           <Link href="https://github.com/mhdawaisrajput" target="_blank">
             <span className="hover:underline">GitHub</span>
           </Link>
-          <Link href="https://www.linkedin.com/in/mhdawaisrajput/" target="_blank">
+          <Link
+            href="https://www.linkedin.com/in/mhdawaisrajput/"
+            target="_blank"
+          >
             <span className="hover:underline">LinkedIn</span>
           </Link>
           <Link href="https://twitter.com/mhdawaisrajput" target="_blank">
